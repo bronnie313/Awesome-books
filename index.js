c3164b5b1bccbbcf8c20bfa60b04e57b3120a0a0
Bisaso Ronald <bronnie313@gmail.com>
2023-05-01 12:00:06 +0300

    get access to the Id's

---
+++ index.js
@@ -0,0 +1,4 @@
+const add = document.getElementById('add')
+const title = document.getElementById('title')
+const author = document.getElementById('author')
+const bookList = document.getElementById('book-list')

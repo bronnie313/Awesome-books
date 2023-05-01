11fb2d8c0e21d326891e861a844314f006529c9f
Bisaso Ronald <bronnie313@gmail.com>
2023-05-01 12:03:01 +0300

    creat class BookBinding

--- index.js
+++ index.js
@@ -9,3 +9,47 @@ class Book {
         this.Author = Author
     }
 }
+
+class BookBinding {
+    constructor() {
+        this.awesomeBooks = localStorage.getItem('Books') ? JSON.parse(localStorage.getItem('Books')) : [];
+    }
+
+    add (title, author) {
+        const book = new Book(title.value, author.value)
+        if(title.value.length && author.value.length) {
+            this.awesomeBooks.push(book)
+        }
+        localStorage.setItem('Books', JSON.stringify(this.awesomeBooks));
+        title.value = '';
+        author.value = '';
+    }
+
+    removeBook (index) {
+        this.awesomeBooks.splice(index, 1)  
+        localStorage.setItem('Books', JSON.stringify(this.awesomeBooks));
+    }
+
+    static update () {
+        const awesomeBooks = JSON.parse(localStorage.getItem('Books'))
+        const bookElement = document.createElement('bookElement')
+        bookList.innerHTML = '';
+        awesomeBooks.forEach((book, i) => {
+            let color;
+            if (i % 2 === 0) {
+                color = 'bg-light'
+            } else {
+                color = '';
+            }
+            bookElement.innerHTML += `
+            <div id="items" class="d-flex justify-content-between align-items-center ${color}">
+                <p class="m-0">"${book.Title}" by ${book.Author}</p>
+                <button type="button" class="removeBtn btn btn-outline-dark p-1" data-index="${i}">Remove</button>
+            </div>
+            `;
+            bookList.appendChild(bookElement)
+        })
+    }
+}
+
+const bookBinding = new BookBinding()

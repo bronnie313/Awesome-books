bb823be320591f34a8c53a01ee5ba6e55581c7ea
Bisaso Ronald <bronnie313@gmail.com>
2023-05-01 12:01:18 +0300

    create class Book

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const add = document.getElementById('add')
 const title = document.getElementById('title')
 const author = document.getElementById('author')
 const bookList = document.getElementById('book-list')
+
+class Book {
+    constructor(Title, Author) {
+        this.Title = Title
+        this.Author = Author
+    }
+}
